108c371c97f188b255ed0fd5196f9b8cfad59629
Bagus Valentino <bagusvalentino.dev@gmail.com>
2025-03-21 19:22:27 +0700

    🐛 Fix base url configuration for production

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,6 +2,6 @@ import { createAuthClient } from 'better-auth/react'
 import { emailOTPClient, twoFactorClient } from 'better-auth/client/plugins'
 
 export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.BASE_URL,
   plugins: [emailOTPClient(), twoFactorClient()]
 })
